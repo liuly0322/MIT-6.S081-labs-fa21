e8f496095f398261d83a814e1d6d3b58c75402f5
liuly0322 <liuly0322@mail.ustc.edu.cn>
2023-04-15 08:34:56 +0000

    checkpoint(lab2): finish trace syscall

--- kernel/proc.c
+++ kernel/proc.c
@@ -164,6 +164,7 @@ freeproc(struct proc *p)
   p->killed = 0;
   p->xstate = 0;
   p->state = UNUSED;
+  p->trace_mask = 0;
 }
 
 // Create a user page table for a given process,
@@ -291,6 +292,9 @@ fork(void)
 
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
+
+  // copy trace mask number.
+  np->trace_mask = p->trace_mask;
 
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
