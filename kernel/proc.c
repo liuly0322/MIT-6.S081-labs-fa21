319152fa21ab96d51c9d1fbee60d3260b163df76
liuly0322 <liuly0322@mail.ustc.edu.cn>
2023-04-17 02:57:18 +0000

    Speed up system calls

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,14 @@ found:
     return 0;
   }
 
+  // Allocate a usyscall page and store process struct data.
+  if((p->usyscall = (struct usyscall *)kalloc()) == 0) {
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  p->usyscall->pid = p->pid;
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -196,6 +204,13 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // map usyscall just below trapframe
+  if (mappages(pagetable, USYSCALL, PGSIZE, (uint64)(p->usyscall), PTE_U | PTE_R) < 0) {
+    uvmunmap(pagetable, USYSCALL, 1, 0); // TODO: explain why
+    uvmfree(pagetable, 0); // TODO: explain why
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -206,6 +221,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
